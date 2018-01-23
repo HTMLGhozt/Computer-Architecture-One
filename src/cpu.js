1a720da66bf6ced53aedf7f19e2dbb990a9f2185
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-01-23 14:01:23 -0700

    refactored to air-bnb standards.

--- src/cpu.js
+++ src/cpu.js
@@ -1,161 +1,192 @@
 /**
  * LS-8 v2.0 emulator skeleton code
- */
+*/
 
-const fs = require('fs');
+// const fs = require('fs');
 
 // Instructions
-
-const HLT = 0b00011011; // Halt CPU
-// !!! IMPLEMENT ME
-const LDI = 0b00000100; // LDI
-const MUL = 0b00000101; // MUL
-const PRN = 0b00000110; // PRN
+const HLT = 0b00011011; // HLT
+const LDI = 0b00000100; // LDI register immediate
+const MUL = 0b00000101; // MUL registerA registerB
+const PRN = 0b00000110; // PRN register *pseudo-insctruction*
+const PSH = 0b00001010; // PSH register
+const POP = 0b00001011; // POP register
+const NOP = 0b00000000; // NOP
 
 /**
  * Class for simulating a simple Computer (CPU & memory)
- */
+*/
 class CPU {
+  /**
+   * Initialize the CPU
+    */
+  constructor(ram) {
+    this.ram = ram;
+    this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
+    this.reg[7] = 0xF8; // Stack Pointer (SP)
 
-    /**
-     * Initialize the CPU
-     */
-    constructor(ram) {
-        this.ram = ram;
+    // Special-purpose registers
+    this.reg.PC = 0; // Program Counter
+    this.reg.IR = 0; // Instruction Register
 
-        this.reg = new Array(8).fill(0); // General-purpose registers
-        
-        // Special-purpose registers
-        this.reg.PC = 0; // Program Counter
-        this.reg.IR = 0; // Instruction Register
+    this.setupBranchTable();
+  }
 
-		this.setupBranchTable();
+  /**
+   * Sets up the branch table
+   */
+  setupBranchTable() {
+    const bt = {};
+    bt[HLT] = this.HLT;
+    bt[LDI] = this.LDI;
+    bt[MUL] = this.MUL;
+    bt[PRN] = this.PRN;
+    bt[PSH] = this.PSH;
+    bt[POP] = this.POP;
+    bt[NOP] = this.alu;
+    this.branchTable = bt;
+  }
+
+  /**
+   * Store value in memory address, useful for program loading
+    */
+  poke(address, value) {
+    this.ram.write(address, value);
+  }
+  /**
+   * a helper function that increments the Program Counter (PC)
+    * and returns the instructions loaded in ram.
+    * @param {number} n - a positive number.
+    */
+  helper(n) {
+    const arr = [];
+    for (let i = 0; i < n; i++) {
+      this.reg.PC += 1;
+      arr.push(this.ram.read(this.reg.PC));
     }
-	
-	/**
-	 * Sets up the branch table
-	 */
-	setupBranchTable() {
-		const bt = {}
-        bt[HLT] = this.HLT;
-        // !!! IMPLEMENT ME            
-        bt[LDI] = this.LDI; // LDI
-        bt[MUL] = this.MUL; // MUL
-        bt[PRN] = this.PRN; // PRN
-        this.branchTable = bt;
-	}
+    this.reg.PC += 1;
+    return arr;
+  }
+  /**
+   * Starts the clock ticking on the CPU
+   */
+  startClock() {
+    this.clock = setInterval(() => {
+      this.tick();
+    }, 1);
+  }
 
-    /**
-     * Store value in memory address, useful for program loading
-     */
-    poke(address, value) {
-        this.ram.write(address, value);
+  /**
+   * Stops the clock
+    */
+  stopClock() {
+    clearInterval(this.clock);
+  }
+
+  /**
+   * ALU functionality
+    * @param {string} op can be: ADD SUB MUL DIV INC DEC CMP
+    * @param {number} regA - a positive integer between 1 and 5.
+    * @param {number} regB - a positive integer between 1 and 5.
+    */
+  alu(op = 'NOP', regA, regB) {
+    switch (op) {
+      case 'MUL':
+        this.reg[regA] *= this.reg[regB] & 0b11111111;
+        break;
+      case 'ADD':
+        this.reg[regA] += this.reg[regB] & 0b11111111;
+        break;
+      case 'SUB':
+        this.reg[regA] -= this.reg[regB] & 0b11111111;
+        break;
+      case 'DIV':
+        break;
+      case 'INC':
+        this.reg[regA] += 1 & 0b11111111;
+        break;
+      case 'DEC':
+        this.reg[regA] -= 1 & 0b11111111;
+        break;
+      case 'CMP':
+        break;
+      case 'NOP':
+      default:
+        this.reg.IP += 1 & 0b00000110;
+        break;
     }
+  }
 
-    /**
-     * Starts the clock ticking on the CPU
-     */
-    startClock() {
-        const _this = this;
+  /**
+   * Advances the CPU one cycle
+    */
+  tick() {
+    // Load the instruction register from the current PC
+    this.reg.IR = this.ram.read(this.reg.PC);
 
-        this.clock = setInterval(() => {
-            _this.tick();
-        }, 1);
-    }
+    // console.info(`${this.reg.PC}: ${this.reg.IR.toString(2)}`); // Debugging output
 
-    /**
-     * Stops the clock
-     */
-    stopClock() {
-        clearInterval(this.clock);
-    }
+    // Based on the value in the Instruction Register, jump to the
+    // appropriate hander in the branchTable
+    const handler = this.branchTable[this.reg.IR];
+    // Check that the handler is defined, halt if not (invalid
+    // instruction)
+    if (handler === undefined) this.branchTable[HLT].call(this);
+    // We need to use call() so we can set the "this" value inside
+    // the handler (otherwise it will be undefined in the handler)
+    else handler.call(this);
+  }
 
-    /**
-     * ALU functionality
-     * 
-     * op can be: ADD SUB MUL DIV INC DEC CMP
-     */
-    alu(op, regA, regB) {
-        switch (op) {
-            case 'MUL':
-                // !!! IMPLEMENT ME
-                this.reg[regA] *= this.reg[regB] & 0b11111111;
-                break;
-            case 'ADD':
-            case 'SUB':
-            case 'DIV':
-            case 'INC':
-            case 'DEC':
-            case 'CMP':
-        }
-    }
+  // INSTRUCTION HANDLER CODE:
 
-    /**
-     * Advances the CPU one cycle
-     */
-    tick() {
-        // !!! IMPLEMENT ME
-        // Load the instruction register from the current PC
-        this.reg.IR = this.ram.read(this.reg.PC);
+  /**
+   * HLT
+    */
+  HLT() {
+    this.stopClock();
+  }
 
-        // console.log(`${this.reg.PC}: ${this.reg.IR.toString(2)}`); // Debugging output
+  /**
+   * LDI R,I
+    */
+  LDI() {
+    const [reg, val] = this.helper(2);
+    this.reg[reg] = val;
+  }
 
-        // Based on the value in the Instruction Register, jump to the
-        // appropriate hander in the branchTable
-        const handler = this.branchTable[this.reg.IR];
-        // Check that the handler is defined, halt if not (invalid
-        // instruction)
-        if (handler === undefined) this.branchTable[HLT].call(this);
-        // We need to use call() so we can set the "this" value inside
-        // the handler (otherwise it will be undefined in the handler)
-        else handler.call(this);
-    }
+  /**
+   * MUL R,R
+    */
+  MUL() {
+    const [regA, regB] = this.helper(2);
+    this.alu('MUL', regA, regB);
+  }
 
-    // INSTRUCTION HANDLER CODE:
+  /**
+   * PRN R
+    */
+  PRN() {
+    const [reg] = this.helper(1);
+    console.info(this.reg[reg]);
+  }
 
-    /**
-     * HLT
-     */
-    HLT() {
-        // !!! IMPLEMENT ME
-        this.stopClock();
-        return;
-    }
+  /**
+   * PSH R
+    */
+  PSH() {
+    const [reg] = this.helper(1);
+    this.reg[7] -= 1;
+    this.poke(this.reg[7], this.reg[reg]);
+  }
 
-    /**
-     * LDI R,I
-     */
-    LDI() {
-        // !!! IMPLEMENT ME
-        const [reg, val] = this.HELP(2);
-        this.reg[reg] = val;
-    }
-    HELP(n) {
-        const arr = [];
-        for (let i = 0; i < n; i++) {
-            arr.push(this.ram.read(++this.reg.PC));
-        }
-        this.reg.PC++
-        return arr;
-    }
-    /**
-     * MUL R,R
-     */
-    MUL() {
-        // !!! IMPLEMENT ME
-        const [regA, regB] = this.HELP(2);
-
-        this.alu('MUL', regA, regB);
-    }
-
-    /**
-     * PRN R
-     */
-    PRN() {
-        // !!! IMPLEMENT ME
-        const [reg] = this.HELP(1);
-        console.log(this.reg[reg]);
-    }
+  /**
+   * POP R
+    */
+  POP() {
+    const [reg] = this.helper(1);
+    this.reg[reg] = this.ram.read(this.reg[7]);
+    this.reg[7] += 1;
+  }
 }
 
 module.exports = CPU;
--- src/ls8.js
+++ src/ls8.js
@@ -6,79 +6,74 @@ const CPU = require('./cpu');
  * Process a loaded file
  */
 function processFile(content, cpu, onComplete) {
-    // Pointer to the memory address in the CPU that we're
-    // loading a value into:
-    let curAddr = 0;
-    
-    // Split the lines of the content up by newline
-    const lines = content.split('\n');
+  // Pointer to the memory address in the CPU that we're
+  // loading a value into:
+  let currVal = 0;
+  // Split the lines of the content up by newline
+  const lines = content.split('\n');
 
-    // Loop through each line of machine code
-    for (let line of lines) {
-        // !!! IMPLEMENT ME
-        // Strip comments
-        // Remove whitespace from either end of the line
-        line = line.replace(/#(.*)|\s/g, '');
-        // Ignore empty lines
-        if (line.length === 0) { continue; }
-        // Convert from binary string to numeric value
-        const val = parseInt(line, 2);
-        // Store in the CPU with the .poke() function
-        cpu.poke(curAddr, val);
-        // And on to the next one
-        curAddr++;
+  // Iterate through the lines of content
+  while (lines.length > 0) {
+    // Strip comments and remove whitespace
+    const line = lines.shift().replace(/#(.*)|\s/g, '');
+    // Ignore empty lines
+    if (line.length !== 0) {
+      // Convert from binary string to numeric value
+      const val = parseInt(line, 2);
+      // Store in the CPU with the .poke() function
+      cpu.poke(currVal, val);
+      // next position
+      currVal += 1;
     }
-
-    onComplete(cpu);
+  }
+  onComplete(cpu);
 }
 
 /**
  * Load the instructions into the CPU from stdin
  */
 function loadFileFromStdin(cpu, onComplete) {
-    let content = '';
+  let content = '';
 
-    // Read everything from standard input, stolen from:
-    // https://stackoverflow.com/questions/13410960/how-to-read-an-entire-text-stream-in-node-js
-    process.stdin.resume();
-    process.stdin.on('data', function(buf) { content += buf.toString(); });
-    process.stdin.on('end', () => { processFile(content, cpu, onComplete); });
+  // Read everything from standard input, stolen from:
+  // https://stackoverflow.com/questions/13410960/how-to-read-an-entire-text-stream-in-node-js
+  process.stdin.resume();
+  process.stdin.on('data', (buf) => { content += buf.toString(); });
+  process.stdin.on('end', () => { processFile(content, cpu, onComplete); });
 }
 
 /**
  * Load the instructions into the CPU from a file
  */
 function loadFile(filename, cpu, onComplete) {
-    const content = fs.readFileSync(filename, 'utf-8');
-    processFile(content, cpu, onComplete);
+  const content = fs.readFileSync(filename, 'utf-8');
+  processFile(content, cpu, onComplete);
 }
 
 /**
  * On File Loaded
- * 
+ *
  * CPU is set up, start it running
  */
 function onFileLoaded(cpu) {
-    cpu.startClock();
+  cpu.startClock();
 }
 
 /**
  * Main
  */
-
-let ram = new RAM(256);
-let cpu = new CPU(ram);
+const ram = new RAM(256);
+const cpu = new CPU(ram);
 // Get remaining command line arguments
 const argv = process.argv.slice(2);
-
 // Check arguments
 if (argv.length === 0) {
-    // Read from stdin
-    loadFileFromStdin(cpu, onFileLoaded);
-} else if (argv.length == 1) {
-    // Read from file
-    loadFile(argv[0], cpu, onFileLoaded);
+  // Read from stdin
+  loadFileFromStdin(cpu, onFileLoaded);
+} else if (argv.length === 1) {
+  // Read from file
+  loadFile(argv[0], cpu, onFileLoaded);
 } else {
-    console.error('usage: ls8 [machinecodefile]');
-    process.exit(1);
+  console.error('usage: ls8 [machinecodefile]');
+  process.exit(1);
 }
